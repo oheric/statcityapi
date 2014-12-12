4603c49b7d8b3c3ef3aca7b51598a6bd1c543a80
oheric <ohericyoung@gmail.com>
2014-12-12 14:18:18 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Statcityapi::Application.config.secret_key_base = 'b91bd18ea1b5c35f5383eab82398a561fb6e4f0a1501ea84e0d5459a531ec3174c7a399ddb9d1f20ac275f7c58d29feb87ed74efdbecf99559e7544fcfc05c2c'
